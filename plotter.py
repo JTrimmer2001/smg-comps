65066979c3e277d311932c25933da6d20810fabe
Josh Trimmer <jet2609@gmail.com>
2023-11-29 11:52:33 +0000

    radec plots

--- plotter.py
+++ plotter.py
@@ -62,4 +62,74 @@ def snrplotter():
         plt.clf()
         
 
-snrplotter()
+def radec():
+    cats = gt.trawler('mastercats')
+
+    for i in cats:
+        parts = i.split('_')
+        name = 'mastercats/'+i
+        data = tb.read(name, format='ascii')
+        #mask = (data['RA']>0)&(data['DEC']>0)
+        #fidelity = data[mask]
+
+        if os.path.isdir('plots/radec/plain/'+parts[0]+'/') == False:
+            os.makedirs('plots/radec/plain/'+parts[0]+'/')
+
+        plt.scatter(data['RA'], data['DEC'],s=1)
+        plt.xlabel('RA')
+        plt.ylabel('DEC')
+        plt.savefig('plots/radec/plain/'+parts[0]+'/'+parts[1]+'.png',bbox_inches='tight')
+        plt.clf()
+
+def radecFidelity():
+    cats = gt.trawler('mastercats')
+
+    for i in cats:
+        parts = i.split('_')
+        name = 'mastercats/'+i
+        data = tb.read(name, format='ascii')
+        mask0_6 = (data['F_kw']>0.6)
+        mask0_8 = (data['F_kw']>0.8)
+        fidelity0_6 = data[mask0_6]
+        fidelity0_8 = data[mask0_8]
+
+        if os.path.isdir('plots/radec/wFkw/'+parts[0]+'/') == False:
+            os.makedirs('plots/radec/wFkw/'+parts[0]+'/')
+
+        fig, ax = plt.subplots()
+
+        f00=ax.scatter(data['RA'], data['DEC'],s=0.5,c='0.8',label='F_kw > 0')
+        f06=ax.scatter(fidelity0_6['RA'],fidelity0_6['DEC'],marker='2',c='r',label='F_kw > 0.6')
+        f08=ax.scatter(fidelity0_8['RA'],fidelity0_8['DEC'],marker='1',c='b',label='F_kw > 0.8')
+
+        ax.set(xlabel='RA',ylabel='DEC')
+        ax.legend(handles=[f00,f06,f08])
+
+        fig.savefig('plots/radec/wFkw/'+parts[0]+'/'+parts[1]+'.pdf',bbox_inches='tight')
+        plt.close('all')
+
+def special():
+        data = tb.read('mastercats/aless62_spw2123_clumpsP_minSNR_3.0_cropped_wfidelity_sorted.cat', format='ascii')
+        mask = (data['FREQ_GHZ']>97)&(data['FREQ_GHZ']<98)
+        data = data[mask]
+        mask0_6 = (data['F_kw']>0.6)
+        mask0_8 = (data['F_kw']>0.8)
+        fidelity0_6 = data[mask0_6]
+        fidelity0_8 = data[mask0_8]
+
+        fig, ax = plt.subplots()
+
+        f00=ax.scatter(data['RA'], data['DEC'],s=0.5,c='0.8',label='F_kw > 0')
+        f06=ax.scatter(fidelity0_6['RA'],fidelity0_6['DEC'],marker='2',c='r',label='F_kw > 0.6')
+        f08=ax.scatter(fidelity0_8['RA'],fidelity0_8['DEC'],marker='1',c='b',label='F_kw > 0.8')
+
+        ax.set(xlabel='RA',ylabel='DEC')
+        ax.legend(handles=[f00,f06,f08])
+
+        if os.path.isdir('plots/special/') == False:
+            os.makedirs('plots/special/')
+            
+        fig.savefig('plots/special/aless62_spw2123_radec_freqlimited9798.pdf',bbox_inches='tight')
+        plt.close('all')
+
+special()
